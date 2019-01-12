4828f674792bb338d239d1e10f4ec22d3bc101a5
choiwono <treasure_b@naver.com>
2019-01-12 16:40:11 +0900

    1,2,3 더하기 문제 해결

--- src/main/java/plus123.java
+++ src/main/java/plus123.java
@@ -1,3 +1,23 @@
-public class plus123 {
+import java.util.Scanner;
 
+public class plus123 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            Integer value = scanner.nextInt();
+            int sum = 0;
+            int goal = go(sum,value);
+            System.out.println(goal);
+        }
+    }
+
+    public static int go(int sum, int goal) {
+        if(sum > goal) return 0;
+        if(sum == goal) return 1;
+        int now = 0;
+        for (int i=1; i<=3; i++) {
+            now += go(sum+i,goal);
+        }
+        return now;
+    }
 }
