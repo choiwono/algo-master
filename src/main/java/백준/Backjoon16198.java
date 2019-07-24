2c2568425566afbd28a4e2ed2bf84ff368880c9a
choiwono <treasure_b@naver.com>
2019-07-24 23:44:41 +0900

    # 백준 문제풀이 16198 수정

--- src/main/java/백준/Backjoon16198.java
+++ src/main/java/백준/Backjoon16198.java
@@ -4,23 +4,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Backjoon16198 {
+    static int[] arr = new int[]{1,2,3,4};
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4};
         //boolean check[] = new boolean[4];
-        solution(arr);
+        System.out.println(solution());
     }
 
-    public static int solution(int[] arr){
+    public static int solution(){
         int answer = 0;
-        //Queue<Integer> que = new LinkedList<>();
-        // 1 2 3 4
-        boolean[] check = new boolean[arr.length]; // 0 1 2 3
-        // 0 1
-        /*for(int i=0; i<arr.length; i++)
-            que.add(arr[i]);*/
 
-        //while(true){
-        //int sum = 0;
+        boolean[] check = new boolean[arr.length]; // 0 1 2 3
+
         for(int i=1; i<arr.length-1; i++){
             if(!check[i]){// 1 2
                  // 1 2 3 4, 2을 골랐을 경우 1x3 = 3이 된 상태..
@@ -30,13 +24,7 @@ public class Backjoon16198 {
                 while(check[right]) ++right;
 
                 check[i] = true;
-                // 0 + 1*3 = 3
-                // 3
-                // 1*4 = 4
-                // 0, 0+3
-                // 3, 3 + 4 7 , 7
-                // 7
-                answer = Math.max(answer,answer + arr[left] * arr[right]);
+                answer = Math.max(answer,solution() + arr[left] * arr[right]);
             }
         }
         return answer;
