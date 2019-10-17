baadeb2ac57ee013018a6c172574d1ddd2d3f10f
choiwono <treasure_b@naver.com>
2019-10-17 23:56:36 +0900

    프로그래머스 네트워크 다시풀기

--- src/main/java/프로그래머스/Programmers43162.java
+++ src/main/java/프로그래머스/Programmers43162.java
@@ -1,25 +1,26 @@
 package 프로그래머스;
 
 public class Programmers43162 {
-    public int solution(int n, int[][] computers){
-        int answer = 0;
-        boolean[] chk = new boolean[n];
-        // n 길이만큼 체크배열을 만들었다.
-        for(int i=0; i<n; i++){
-            if(!chk[i]){ // 탐색이 안된경우..!, 탐색이 안됐다는건 결국 서로 연결이 되어있지 않기에 ++
-                dfs(computers,chk,i);
-                answer++;
-            }
-        }
+    
+    static int answer = 0;
+    static boolean[] chk;
+    public static void main(String[] args){
+        
+    }
+
+    public static int solution(int n, int[][] computers){
+        int start = 0;
+        chk = new boolean[n];
+        
+        dfs(computers, chk, start);    
         return answer;
     }
 
-    public void dfs(int[][] computers, boolean[] chk, int start){
-        chk[start] = true;
-        for(int j=0; j<computers.length; j++){
-            if(chk[j] == false && computers[start][j] == 1){
-                dfs(computers,chk,j);
-            }
-        }
+    public static void dfs(int[][] computers, boolean[] chk, int start){
+       if(chk[start]) return;
+       // 탐색완료
+       chk[start] = true;
+
+       
     }
 }
