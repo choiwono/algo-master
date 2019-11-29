cb9532d164b189c618fc5d9ac2287339ee1ed43e
choiwono <treasure_b@naver.com>
2019-11-29 21:07:23 +0900

    Create Programmers12917.java

---
+++ src/main/java/프로그래머스/Programmers12917.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Programmers12917 {
+    public static void main(Strings[]){
+    
+    }
+    
+    public static long solution(int n, int[] works){
+        
+    }
+}
