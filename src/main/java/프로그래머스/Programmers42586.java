bdf6a1f91f2dd9ec66fcc9a862414b9d85f98646
choiwono <treasure_b@naver.com>
2020-01-14 00:27:00 +0900

    재풀이 42586

--- src/main/java/프로그래머스/Programmers42586.java
+++ src/main/java/프로그래머스/Programmers42586.java
@@ -4,8 +4,11 @@ import java.util.*;
 
 public class Programmers42586 {
     public static void main(String[] args) {
-        int[] progress = {40, 93, 30, 55, 60, 65};
-        int[] speeds = {60, 1, 30, 5, 10, 7};
+        //int[] progress = {40, 93, 30, 55, 60, 65};
+        int[] progress = {93,30,55};
+        int[] speeds = {1,30,5};
+        //int[] speeds = {60, 1, 30, 5, 10, 7};
+        // 1, 7, 3, 9, 4, 5
         int[] result = solution(progress,speeds);
         for(int i : result){
             System.out.println(i);
@@ -18,7 +21,7 @@ public class Programmers42586 {
         // 세 번째 기능은 55%가 완료되어 있고 하루에 5%씩 작업이 가능하므로 9일간 작업 후 배포가 가능합니다.
         // 따라서 7일째에 2개의 기능, 9일째에 1개의 기능이 배포됩니다.
         Queue<Integer> que = new LinkedList<>();
-        
+        ArrayList<Integer> list = new ArrayList<>();
         // 7 / 1 = 
         for(int i=0; i<progresses.length; i++){
             int rest = 100 - progresses[i];
@@ -26,8 +29,37 @@ public class Programmers42586 {
             if(rest % speeds[i] > 0){
                 deployDay++;
             } 
-            System.out.println(deployDay);
+            que.add(deployDay);
         }
-        return new int[2];
+
+        int deploy = 1;
+        int poll = que.poll();
+        while(true){
+            
+            // 1, 7, 3, 9, 4, 5
+            // 1번째놈, 2번째놈
+            System.out.println(poll+","+deploy);
+            if(poll >= que.peek()){
+                deploy++;
+                que.poll();
+            } else {
+                list.add(deploy);
+                poll = que.peek();
+                que.poll();
+                deploy = 1;
+            }
+
+            if(que.isEmpty()){
+                list.add(deploy);
+                break;
+            }
+        }
+
+        int[] result = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++) {
+            result[i] = list.get(i);    
+        }
+        return result;
     }
 }
