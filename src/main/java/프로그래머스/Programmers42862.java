889860a99209a49ab6e74896a976bc5c34ec9f00
choiwono <treasure_b@naver.com>
2020-01-31 00:06:18 +0900

    프로그래머스 체육복 문제풀이 ( 예외케이스 있음..)

--- src/main/java/프로그래머스/Programmers42862.java
+++ src/main/java/프로그래머스/Programmers42862.java
@@ -1,47 +1,48 @@
 package 프로그래머스;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class Programmers42862 {
     public static void main(String[] args) {
-        int n = 5;
-        int[] lost = {2,4};
-        int[] reserve = {3};
+        int n = 7;
+        int[] lost = {2,3,4};
+        int[] reserve = {1,2,3,6};
         System.out.println(solution(n,lost,reserve));
     }
     public static int solution(int n, int[] lost, int[] reserve){
-        int answer = n - lost.length;
-        // 잃어버린 놈 == 여분 가지고 있는놈은 제외해야됨
+        //int answer = 0;
+        int noCloth = lost.length;
+        boolean[] check = new boolean[reserve.length];
+        // 정렬로 순서를 맞춘다
         Arrays.sort(lost);
         Arrays.sort(reserve);
+        // 3,4,5
+        // 3,5,7
 
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    lost[i] = -2;
-                    reserve[j] = -2;
+        
+        for(int i=0; i<lost.length; i++) {
+            
+            for(int j=0; j<reserve.length; j++) {
+                // 잃어버린 놈 == 가져온놈이 같은경우
+                if(lost[i] == reserve[j] && !check[j]) {
+                    System.out.println(lost[i]+","+reserve[j]);
+                    noCloth--;
+                    check[j] = true;
+                    break;
+                } else {
+                    if(!check[j]) {
+                        if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                            System.out.println(lost[i] + "," + reserve[j]);
+                            check[j] = true;
+                            noCloth--;
+                            break;
+                        }
+                    }
                 }
             }
         }
+        //System.out.println(noCloth);
 
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] + 1 == reserve[j] || lost[i] - 1 == reserve[j]){
-                    lost[i] = -1;
-                    reserve[j] = -2;
-                }
-            }
-        }
-
-        for(int i=0; i<lost.length; i++){
-            if(lost[i] == -1 || lost[i] == -2){
-                answer++;
-            }
-        }
-
-        return answer;
+        return n - noCloth;
     }
 }
